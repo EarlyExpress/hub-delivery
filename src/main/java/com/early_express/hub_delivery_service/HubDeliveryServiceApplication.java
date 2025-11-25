b23d5c1c3a4772367d8aef3b5eb5a1d6998ae3da
jun <pickjog@gmail.com>
2025-11-25 09:39:32 +0900

    first commit

---
+++ src/main/java/com/early_express/hub_delivery_service/HubDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.early_express.hub_delivery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/early_express/hub_delivery_service/HubDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.early_express.hub_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
